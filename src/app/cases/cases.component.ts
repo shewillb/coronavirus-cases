9bd44e76ed7947b22b3f021c132735ee22228211
shewillb <62865240+shewillb@users.noreply.github.com>
2020-03-30 17:11:44 +0800

    add sorting

--- src/app/cases/cases.component.ts
+++ src/app/cases/cases.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ApiService } from '../api.service';
 import { Cases } from '../models/cases';
+import { MatSort } from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
 
 
 @Component({
@@ -12,18 +14,24 @@ export class CasesComponent implements OnInit {
 
   constructor( private api: ApiService) { }
   displayedColumns: string[] = ['countryregion', 'confirmed', 'deaths', 'recovered'];
-  data: Cases[] = [];
+  data: any;
   isLoadingResults = true;
+
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+
   ngOnInit(): void {
+    // this.data.sort = this.sort;
     this.api.getCases()
     .subscribe((res: any) => {
-      this.data = res;
+      this.data = new MatTableDataSource(res);
+      this.data.sort = this.sort;
       console.log(this.data);
       this.isLoadingResults = false;
     }, err => {
       console.log(err);
       this.isLoadingResults = false;
     });
+
   }
 
 }
