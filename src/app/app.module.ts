9be1bc13153af2b606b1c835955ad52566d7b370
shewillb <62865240+shewillb@users.noreply.github.com>
2020-03-30 11:50:37 +0800

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CasesComponent } from './cases/cases.component';
+import { CasesDetailsComponent } from './cases-details/cases-details.component';
+import { AddCasesComponent } from './add-cases/add-cases.component';
+import { EditCasesComponent } from './edit-cases/edit-cases.component';
+import { CasesStatComponent } from './cases-stat/cases-stat.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CasesComponent,
+    CasesDetailsComponent,
+    AddCasesComponent,
+    EditCasesComponent,
+    CasesStatComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
